5738bc22364a574f5e34413c703f7e62844ec239
danispinxo <genericpronoun@gmail.com>
2022-06-21 14:15:40 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,16 +1,22 @@
-const raining = true;
-const temperature = 12;
+const raining = false;
+const temperature = 18;
 
 if (raining) {
   console.log("Don't forget your umbrella!");
+} else if (!raining) {
+  console.log("Leave your umbrella at home!");
 }
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
   console.log("Short sleeves won't cut it!");
-} else {
+} else if (temperature > 15 && temperature < 40) {
   console.log("Short sleeves are fine.");
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
 }
 
 console.log("Now you're ready to go outside!");
